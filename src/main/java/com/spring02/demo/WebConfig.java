d1b8bc30f16ef31c18084e5ab4f114e49248a342
milasantacruz <milalovechaton@gmail.com>
2023-01-14 21:38:58 -0300

    add cors

--- src/main/java/com/spring02/demo/WebConfig.java
+++ src/main/java/com/spring02/demo/WebConfig.java
@@ -18,7 +18,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     public void addCorsMappings(CorsRegistry registry) {
 
-        registry.addMapping("/**").allowedOrigins("https://iportfolio.onrender.com");
+        registry.addMapping("/**").allowedOrigins("https://http://localhost:4200");
 
     }
 }
