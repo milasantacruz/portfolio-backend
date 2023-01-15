ef68e094e432f2d7415e9deb1127bc52de58bee8
milasantacruz <milalovechaton@gmail.com>
2023-01-15 00:41:10 -0300

    configuracion de cors nuevamente

--- src/main/java/com/spring02/demo/WebConfig.java
+++ src/main/java/com/spring02/demo/WebConfig.java
@@ -1,17 +1,15 @@
+package com.spring02.demo;
+
 import org.springframework.context.annotation.Configuration;
 
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
-
-@EnableWebMvc
-
 @Configuration
-
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
